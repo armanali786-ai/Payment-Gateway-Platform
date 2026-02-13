aed352055dc5341e970eeedee8e4f12df5f618e5
KrishKalavadia <krish55kalavadia@gmail.com>
2026-02-13 20:07:19 +0530

    Initial commit - Payment Gateway Platform

--- sdk/setup.py
+++ sdk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nexapay',
+    name='nexapay-sdk',
     version='1.0.0',
     packages=find_packages(),
     install_requires=['requests'],
